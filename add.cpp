4dc3411058c896b5165c04298cae955efe570a9a
bscs14f2557 <bscs14f25.57@gmail.com>
2026-03-16 13:25:34 +0500

    sum of two numbers without user input

---
+++ add.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num1 = 5;
+	int num2 = 4;
+	int sum;
+	sum = num1 + num2;
+	cout<<"Sum of given numbers is : "<<sum<<endl;
+	return 0;
+}
