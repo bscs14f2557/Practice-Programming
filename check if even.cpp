fcafbcb27dac2225c955932e2dc70835ed611cf7
bscs14f2557 <bscs14f25.57@gmail.com>
2026-03-19 20:51:52 +0500

    Add files via upload
    check if a number is even or odd without user input

---
+++ check if even.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num=5;
+	if(num%2==0)
+	{
+		cout<<num<<" is even number. "<<endl;
+	}
+	else{
+		cout<<num<<" is odd number."<<endl;
+	}
+	return 0;
+}
