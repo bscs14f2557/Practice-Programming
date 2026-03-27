9b0151d9660eb6bd6de307595c8e5d4f84dc04fd
bscs14f2557 <bscs14f25.57@gmail.com>
2026-03-27 23:21:28 +0500

    dividing two numbers without user input

---
+++ divide.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a=10;
+	int b=5;
+	int divide = a/b;
+	cout<<"Dividing "<<a<<" by "<<b<< " is equal to :  "<<divide<<endl;
+	return 0;
+}
